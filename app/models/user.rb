6e28f902b81864ea8d11b4474473b676d22433a0
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-08-17 13:20:00 -0400

    Modified User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, only_integer: true }
-
-  def recent_posts(limit: 5)  # Set a default value for the limit
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  def recent_posts(limit: 5)
     posts.order(created_at: :desc).limit(limit)
   end
 end
