ef98f69a5e6b62550af32f1e1f31c3a3e3813d76
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-08-17 13:12:43 -0400

    Modified User Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,4 +25,35 @@ RSpec.describe User, type: :model do
     user = User.new(name: 'John Doe', posts_counter: 0)
     expect(user).to be_valid
   end
+
+  describe '#recent_posts' do
+    it 'returns an empty array when the user has no posts' do
+      user = User.create(name: 'Jane Smith', posts_counter: 0)
+      expect(user.recent_posts).to eq([])
+    end
+
+    it 'returns the correct number of recent posts' do
+      user = User.create(name: 'Alice Johnson', posts_counter: 3)
+      posts = []
+      3.times { posts << user.posts.create(title: 'Post Title', content: 'Post Content') }
+
+      expect(user.recent_posts.count).to eq(3)
+    end
+
+    it 'returns posts in descending order of creation' do
+      user = User.create(name: 'Bob Brown', posts_counter: 2)
+      post1 = user.posts.create(title: 'Post 1', content: 'Content 1', created_at: 1.day.ago)
+      post2 = user.posts.create(title: 'Post 2', content: 'Content 2', created_at: 2.days.ago)
+
+      expect(user.recent_posts).to eq([post1, post2])
+    end
+
+    it 'returns only the specified number of recent posts' do
+      user = User.create(name: 'Eve White', posts_counter: 4)
+      posts = []
+      5.times { posts << user.posts.create(title: 'Post Title', content: 'Post Content') }
+
+      expect(user.recent_posts(limit: 3).count).to eq(3)
+    end
+  end
 end
