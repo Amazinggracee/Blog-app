137cd6c71f68f5f46c55ac3e9986e5fd3b4c3760
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-08-17 04:20:36 -0400

    Added more validation for post and also some test

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,9 @@ class Post < ApplicationRecord
 
   before_save :update_author_posts_counter
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :content, presence: true
 
   def recent_comments
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe 'update_author_posts_counter' do
-    it "updates the author's posts_counter" do
+  describe 'validations' do
+    it 'is valid with valid attributes' do
       user = User.create(name: 'John Doe')
-      post = user.posts.create(title: 'Sample Title', content: 'Sample Content')
+      post = user.posts.new(title: 'Sample Title', content: 'Sample Content', comments_counter: 0, likes_counter: 0)
+      expect(post).to be_valid
+    end
 
-      expect do
-        post.author.posts.create(title: 'Another Title', content: 'Another Content')
-      end.to change { user.reload.posts_counter }.by(1)
+    it 'is not valid without a title' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: nil, content: 'Sample Content', comments_counter: 0, likes_counter: 0)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid if title exceeds 250 characters' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'A' * 251, content: 'Sample Content', comments_counter: 0, likes_counter: 0)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a negative comments_counter' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'Sample Title', content: 'Sample Content', comments_counter: -1, likes_counter: 0)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a non-integer comments_counter' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'Sample Title', content: 'Sample Content', comments_counter: 5.5, likes_counter: 0)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a negative likes_counter' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'Sample Title', content: 'Sample Content', comments_counter: 0, likes_counter: -1)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a non-integer likes_counter' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'Sample Title', content: 'Sample Content', comments_counter: 0, likes_counter: 5.5)
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without content' do
+      user = User.create(name: 'John Doe')
+      post = user.posts.new(title: 'Sample Title', content: nil, comments_counter: 0, likes_counter: 0)
+      expect(post).not_to be_valid
     end
   end
 end
